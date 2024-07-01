18fef7ecb0cb2a1441a25a5fe2f6ba9d166a4188
Zahid Hasan <megamind.d.evilll@gmail.com>
2024-07-02 01:33:17 +0600

    Improve Mock Object reporting with clear error message

--- UnitTestNetworkingTests/MockURLSession.swift
+++ UnitTestNetworkingTests/MockURLSession.swift
@@ -22,11 +22,48 @@ class MockURLSession: UrlSessionProtocol{
     func verifyDataTask(with request: URLRequest,
                         file: StaticString = #file,
                         line: UInt = #line){
-        XCTAssertEqual(dataTaskCallCount, 1, "call out", file: file, line: line)
+        
+        guard dataTaskWasCalledOnce(file: file, line: line) else {return}
         XCTAssertEqual(dataTaskArgsRequest.first, request, "request", file: file, line: line)
     }
+    
+    private func dataTaskWasCalledOnce(
+        file: StaticString = #file,
+        line: UInt = #line) -> Bool{
+            return verifyMethodCalledOnce(
+                methodName: "dataTask(with:completionHandler", 
+                callCount: dataTaskCallCount,
+                describeArguments: "request: \(dataTaskArgsRequest)")
+        }
 }
 
 private class DummyURLSessionDataTask: URLSessionDataTask {
     override func resume() {}
 }
+
+func verifyMethodCalledOnce(
+    methodName: String,
+    callCount: Int,
+    describeArguments: @autoclosure () -> String,
+    file: StaticString = #file,
+    line: UInt = #line) -> Bool {
+        
+        if callCount == 0 {
+            XCTFail(
+                "Wanted but not invoked: \(methodName)",
+                file: file,
+                line: line)
+            
+            return false
+        }
+        
+        if callCount > 1{
+            XCTFail("Wanted 1 time but was called \(callCount) times " + "\(methodName) with \(describeArguments())",
+                    file: file,
+                    line: line)
+            return false
+        }
+        
+        return true
+        
+    }
